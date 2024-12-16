495aaacecacba603c3d6999230500c1f4c3e5ac1
suveerbabu <suveerprasad100@gmail.com>
2024-12-16 21:46:34 +0530

    frontend part complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,10 @@ import UserProtectWrapper from './pages/UserProtectWrapper'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 import CaptainLogout from './pages/CaptainLogout'
+import Riding from './pages/Riding'
+import CaptainRiding from './pages/CaptainRiding'
+import UserLogout from './pages/UserLogout'
+import 'remixicon/fonts/remixicon.css'
 
 const App = () => {
     return (
@@ -17,7 +21,10 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Start />} />
           <Route path='/login' element={<UserLogin />} />
+          <Route path='/logout' element={<UserLogout />} />
           <Route path='/signup' element={<UserSignup />} />
+          <Route path = '/riding' element = {<Riding />} />
+          <Route path = '/captain-riding' element = {<CaptainRiding />} />
           <Route path='/captain-login' element={<CaptainLogin />} />
           <Route path='/captain-signup' element={<CaptainSignup />} />
           <Route path='/captain-logout' element={<CaptainLogout />} />
